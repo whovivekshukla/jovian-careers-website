bfdd30e4e2161119c2898944e5782ffc176d8a0a
Vivek <93597098+whovivekshukla@users.noreply.github.com>
2022-09-11 14:27:21 +0000

    added contact page

--- app.py
+++ app.py
@@ -110,5 +110,16 @@ def list_jobs():
   return jsonify(JOBS)
 
 
+@app.route("/contact")
+def contact():
+  
+  return render_template('contact.html', companyname='Jovian')
+
+@app.route("/success", methods=["POST"])
+def success():
+  
+  return render_template('success.html', companyname='Jovian')
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
